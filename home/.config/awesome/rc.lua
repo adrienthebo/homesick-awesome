09263dad11581672d502dae37b05c3cf6ac90a9f
Adrien Thebo <git@somethingsinistral.net>
2015-09-10 14:07:06 -0700

    set border to burnt orange

--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -461,6 +461,6 @@ client.connect_signal("manage", function (c, startup)
 end)
 
 -- client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
-client.connect_signal("focus", function(c) c.border_color = "#00a060" end)
+client.connect_signal("focus", function(c) c.border_color = "#FF9800" end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
