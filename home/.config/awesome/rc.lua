b6ab236d81bab93b9be07d2491890019200515db
Adrien Thebo <git@somethingsinistral.net>
2015-09-10 14:04:03 -0700

    float hangouts windows

--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -377,12 +377,14 @@ awful.rules.rules = {
                      raise = true,
                      keys = clientkeys,
                      buttons = clientbuttons } },
-    { rule = { class = "MPlayer" },
+
+    -- Google hangouts running under Chrome
+    { rule = { instance = "crx_nckgahadagoaajjgafhacjanaoiihapd" },
       properties = { floating = true } },
+
     { rule = { class = "pinentry" },
       properties = { floating = true } },
-    { rule = { class = "gimp" },
-      properties = { floating = true } },
+
     -- Set Firefox to always map on tags number 2 of screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { tag = tags[1][2] } },
